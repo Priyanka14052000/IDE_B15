27157bfac69ef681661299923220b508807c0489
Priyanka14052000 <58840275+Priyanka14052000@users.noreply.github.com>
2020-08-24 16:44:56 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,16 @@
+i1 = int(input("enter first number\n"))
+i2 = int(input("enter second number\n"))
+print(f'i1 = {i1}\ni2 = {i2}')
+print('addition of i1 and i2 is ', i1 + i2)
+
+print('subtraction of i1 and i2 is', i1 - i2)
+
+print('multiplication of i1 and i2 is', i1 * i2)
+
+print('division of i1 and i2', i1 / i2)
+
+print('floor division of i1 and i2 is', i1 // i2)
+
+print('modular division of i1 and i2 is', i1 % i2)
+
+print('power of i1 and i2', i1 ** i2)
